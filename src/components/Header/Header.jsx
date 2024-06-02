2d4036a20452c1a63d8047ed4cd861c53c3977bb
BanuNathy <banu080419@gmail.com>
2024-06-02 21:55:13 +0530

    Did some changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className='header'>
         <div className='header-contents'>
-            <h2>Order your favourite food here</h2>
+            <h2>Welcome to Digital Diner</h2>
             <p>Choose from a diverse menu featuring a delectable array of dishes satisfy your cravings and elevate your dining experience</p>
             <button>View Menu</button>
         </div>     
